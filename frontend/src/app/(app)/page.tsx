88c9adf0e5a7d23868792664bf0de48a05dfeff4
onebignick <nicholas.zhengkai.ong@gmail.com>
2024-10-15 22:30:25 +0800

    scaffold home page

--- frontend/src/app/(app)/page.tsx
+++ frontend/src/app/(app)/page.tsx
@@ -1,12 +1,33 @@
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { currentUser } from "@clerk/nextjs/server";
 
 export default async function Home() {
-  const user = await currentUser()
-  console.log(user);
 
   return (
-    <main>
-      Welcome back 
+    <main className="flex">
+      <HomePanel/>
     </main>
   );
+}
+
+async function HomePanel() {
+  const user = await currentUser()
+
+  return (
+    <section>
+      <Card>
+        <CardHeader>
+          <CardTitle>
+            Welcome back, {user!.username}!
+          </CardTitle>
+          <CardDescription>
+            What would you like to do today?
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <CardTitle>Upcoming Events</CardTitle>
+        </CardContent>
+      </Card>
+    </section>
+  )
 }
