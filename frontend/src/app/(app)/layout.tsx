d69a8bba4aa3f6f3614ed2e74c886dadd364c0df
xinlin2 <xl.cheng.2023@scis.smu.edu.sg>
2024-10-17 20:25:14 +0800

    update footer

--- frontend/src/app/(app)/layout.tsx
+++ frontend/src/app/(app)/layout.tsx
@@ -12,6 +12,10 @@ export default function RootLayout({
       <body className={cn("min-h-screen bg-background text-foreground font-sans antialiased", fontSans.variable)}>
         <Navbar className="mx-6" />
         {children}
+        {/* Footer Section */}
+        <footer className="fixed bottom-0 border-t p-4 w-full text-center bg-[#99DDCD]">
+            <p>© 2024 IS216 MeetGrid Project. All Rights Reserved.</p>
+        </footer>
       </body>
     </html>
   );
