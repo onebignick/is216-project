341397ce1136efe67679e26c6e10f75512a4dfcb
Cloudieunnie <zhirui.wong.2023@scis.smu.edu.sg>
2024-10-17 19:39:50 +0800

    Update EventService.ts
    added randomised 6 digit code into here, still gotta add to check if code is alr in database or not?

--- frontend/src/server/service/EventService.ts
+++ frontend/src/server/service/EventService.ts
@@ -8,6 +8,19 @@ export class EventService {
         this.eventRepository = new EventRepository();
     }
 
+    generateRandomCode(): string{
+        const chr = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+        let code = '';
+        const min = 0;
+        const max = chr.length;
+        for(let i = 0;i<6;i++){
+            const index = Math.floor(Math.random() * (max-min) + min); //generates random index
+            code += chr[index]; //adds chr into code lol
+        }
+        console.log('generated code');
+        return code;
+    }
+
     async getOneEventById(eventId: string) {
         try {
             console.log(eventId);
@@ -33,7 +46,10 @@ export class EventService {
                 return ""
             } else {
                 console.log("Event created successfully")
-                return result[0].id;
+                // return the unique code here too
+                // add to check if code is actually unique?
+                let unique_code = this.generateRandomCode();
+                return {id: result[0].id, code:unique_code};
             }
         } catch (e) {
             console.log(e.message);
