3967751574a578ea97f1143bada75f719e51326c
onebignick <nicholas.zhengkai.ong@gmail.com>
2024-10-17 11:21:31 +0800

    fix event async controller

--- frontend/src/server/controller/EventController.ts
+++ frontend/src/server/controller/EventController.ts
@@ -11,7 +11,7 @@ export class EventController {
     async handleEventCreation(request: Request) {
         try {
             const newEvent = await request.json();
-            const result = this.eventService.createOneEvent(newEvent);
+            const result = await this.eventService.createOneEvent(newEvent);
             return NextResponse.json({ message: "success", result: result}, { status: 200 })
         } catch {
             return NextResponse.json({ message: "An Error occured"}, { status: 500 })
