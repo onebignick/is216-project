911083ae29ce13698b17f6f1e3360c7ca4c531e4
xinlin2 <xl.cheng.2023@scis.smu.edu.sg>
2024-10-17 21:09:07 +0800

    update nav bar with logo

--- frontend/src/components/navbar/navigation-bar.tsx
+++ frontend/src/components/navbar/navigation-bar.tsx
@@ -2,10 +2,11 @@ import { NavigationItem } from "./navigation-item"
 import { NavigationMenu, NavigationMenuList } from "../ui/navigation-menu"
 import { ModeToggle } from "@/components/toggle-theme-button";
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import Logo from "@/assets/logo.svg"
 
 export function Navbar({}: React.HTMLAttributes<HTMLElement>) {
-	const navigationItems: NavigationItem[] = [
-		{href: "/", description: "Meetgrid"},
+	const navigationItems: NavigationItem[] = [ 
+		{href: "/", description: "Meetgrid", icon: Logo},
 		{href: "/event", description: "Your Events"},
 		{href: "/event/create", description: "Create an event"},
 		{href: "/event/register", description: "Register"},
@@ -15,7 +16,7 @@ export function Navbar({}: React.HTMLAttributes<HTMLElement>) {
 		<NavigationMenu className="flex justify-between min-w-full h-[60px] px-5 bg-[#B7DFED]">
 			<NavigationMenuList className="flex gap-4 justify-start">
 				{navigationItems.map((navigationItem: NavigationItem, index: number) => {
-					return <NavigationItem key={index} href={navigationItem.href} description={navigationItem.description}/>
+					return <NavigationItem key={index} href={navigationItem.href} description={navigationItem.description} icon={navigationItem.icon} />
 				})}
 			</NavigationMenuList>
 			<NavigationMenuList className="flex gap-4 justify-end">
--- frontend/src/components/navbar/navigation-item.tsx
+++ frontend/src/components/navbar/navigation-item.tsx
@@ -1,15 +1,26 @@
 import Link from "next/link";
 import { NavigationMenuItem } from "../ui/navigation-menu";
+import Image from "next/image";
 
 export interface NavigationItem {
     href: string;
     description: string;
+    icon?: string; // Optional icon property
 }
 
-export function NavigationItem({ href, description }: NavigationItem) {
+export function NavigationItem({ href, description, icon }: NavigationItem) {
     return (
         <NavigationMenuItem>
-            <Link href={href}>{description}</Link>
+            <Link href={href}> 
+                {icon ? (
+                    <div className="p-1 inline-flex items-center"> {/* Change to inline-flex */}
+                        <Image src={icon} alt={`${description} logo`} className="h-8 w-8" /> {/* Adjust size as needed */}
+                        <span className="ml-2 font-bold">{description}</span> {/* Add margin for spacing */}
+                    </div>
+                ) : (
+                    <span>{description}</span>// Show description if no icon
+                )}
+            </Link>
         </NavigationMenuItem>
     )
 }
