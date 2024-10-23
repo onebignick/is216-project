1b7e06fc8145c23543931e110ba9f79338c13bc6
xinlin2 <xl.cheng.2023@scis.smu.edu.sg>
2024-10-23 23:01:24 +0800

    update frontend register event form -> database not done yet

--- frontend/src/components/forms/register-event-form.tsx
+++ frontend/src/components/forms/register-event-form.tsx
@@ -12,16 +12,24 @@ import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
 import { CalendarIcon } from "lucide-react";
 import { cn } from "@/lib/utils";
 import { Calendar } from "../ui/calendar";
-import { Textarea } from "../ui/textarea";
-import { MeetgridEvent } from "@/server/entity/event";
 import { useUser } from "@clerk/nextjs";
 
 const formSchema = z.object({
-    eventName: z.string(),
-    description: z.string(),
-    startDate: z.date(),
-    endDate: z.date(),
+    eventCode: z.string(),
+    name: z.string(),
+    Date: z.date(),
+    timeslot: z.string() // may need to change later
 })
+
+const availableTimeSlots = [ // sample timeslot. need to get from database later
+    "09:00 AM - 10:00 AM",
+    "10:00 AM - 11:00 AM",
+    "11:00 AM - 12:00 PM",
+    "01:00 PM - 02:00 PM",
+    "02:00 PM - 03:00 PM",
+    "03:00 PM - 04:00 PM",
+    "04:00 PM - 05:00 PM",
+];
 
 export function RegisterEventForm() {
     const { user } = useUser();
@@ -31,27 +39,9 @@ export function RegisterEventForm() {
     });
 
     async function onSubmit(values: z.infer<typeof formSchema>) {
-        const newEvent: MeetgridEvent = {
-            name: values.eventName,
-            EventCode: null, // temporary testing
-            description: values.description,
-            startDate: values.startDate.toString(),
-            endDate: values.endDate.toString(),
-            organizerId: user!.id,
-            participantNum: null,
-            reminder: null,
-        }
 
-        const response = await fetch("/api/event/create", {
-            method: "POST",
-            body: JSON.stringify(newEvent),
-        })
-        if (response.ok) {
-            const data = await response.json();
-            console.log(data);
-        } else {
-            console.log("An error occured");
-        }
+
+
     }
 
     return(
@@ -59,14 +49,14 @@ export function RegisterEventForm() {
             <Form {...form}>
                 <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
                     <CardHeader>
-                        <CardTitle className = "text-center">Register an event</CardTitle>
+                        <CardTitle className = "text-center">Create an event</CardTitle>
                     </CardHeader>
-                    <CardContent className="flex flex-col gap-4">
+                    <CardContent className="grid grid-cols-2 gap-4">
                         <FormField
                             control={form.control}
-                            name="eventName"
+                            name="eventCode"
                             render={({field}) => (
-                                <FormItem>
+                                <FormItem className="col-span-2">
                                     <FormControl>
                                         <Input placeholder="Event name here" {...field} />
                                     </FormControl>
@@ -76,75 +66,71 @@ export function RegisterEventForm() {
                         />
                         <FormField
                             control={form.control}
-                            name="description"
+                            name="name"
                             render={({field}) => (
-                                <FormItem>
+                                <FormItem className="col-span-2">
                                     <FormControl>
-                                        <Textarea placeholder="Tell us more about your event" {...field}/>
+                                        <Input placeholder="Your Name here" {...field} />
                                     </FormControl>
                                     <FormMessage/>
                                 </FormItem>
                             )}
                         />
-                        <div className="flex justify-evenly">
-                            <FormField
-                                control={form.control}
-                                name="startDate"
-                                render={({field}) => (
-                                    <FormItem>
-                                        <Popover>
-                                            <PopoverTrigger asChild>
-                                                <FormControl>
-                                                    <Button variant="outline" className={cn("w=[240px] pl-3 text-left fonr-normal", !field.value && "text-muted-foreground")}>
-                                                        {field.value ? format(field.value, "PPP") : <span>Pick a start date</span>}
-                                                        <CalendarIcon className="ml-auto h-4 w-4 opacity-50"/>
-                                                    </Button>
-                                                </FormControl>
-                                            </PopoverTrigger>
-                                            <PopoverContent className="w-auto p-0" align="start">
-                                                <Calendar
-                                                    mode="single"
-                                                    selected={field.value}
-                                                    onSelect={field.onChange}
-                                                    disabled={(date) => date > new Date() || date < new Date("1900-01-01")}
-                                                    initialFocus
-                                                />
-                                            </PopoverContent>
-                                        </Popover>
-                                        <FormMessage/>
-                                    </FormItem>
-                                )}
-                            />
-                            <FormField
-                                control={form.control}
-                                name="endDate"
-                                render={({field}) => (
-                                    <FormItem>
-                                        <Popover>
-                                            <PopoverTrigger asChild>
-                                                <FormControl>
-                                                    <Button variant="outline" className={cn("w=[240px] pl-3 text-left fonr-normal", !field.value && "text-muted-foreground")}>
-                                                        {field.value ? format(field.value, "PPP") : <span>Pick an end date</span>}
-                                                        <CalendarIcon className="ml-auto h-4 w-4 opacity-50"/>
-                                                    </Button>
-                                                </FormControl>
-                                            </PopoverTrigger>
-                                            <PopoverContent className="w-auto p-0" align="start">
-                                                <Calendar
-                                                    mode="single"
-                                                    selected={field.value}
-                                                    onSelect={field.onChange}
-                                                    disabled={(date) => date > new Date() || date < new Date("1900-01-01")}
-                                                    initialFocus
-                                                />
-                                            </PopoverContent>
-                                        </Popover>
-                                        <FormMessage/>
-                                    </FormItem>
-                                )}
-                            />
-                        </div>
-                        <Button type="submit">Submit</Button>
+                        <FormField
+                            control={form.control}
+                            name="Date"
+                            render={({field}) => (
+                                <FormItem className="col-span-2 sm:col-span-1">
+                                    <Popover>
+                                        <PopoverTrigger asChild>
+                                            <FormControl>
+                                                <Button variant="outline" className={cn("w-full pl-3 text-left font-normal", !field.value && "text-muted-foreground")}>
+                                                    {field.value ? format(field.value, "PPP") : <span>Pick a date</span>}
+                                                    <CalendarIcon className="ml-auto h-4 w-4 opacity-50"/>
+                                                </Button>
+                                            </FormControl>
+                                        </PopoverTrigger>
+                                        <PopoverContent className="w-auto p-0" align="start">
+                                            <Calendar
+                                                mode="single"
+                                                selected={field.value}
+                                                onSelect={field.onChange}
+                                                disabled={(date) => {
+                                                    const today = new Date();
+                                                    today.setHours(0, 0, 0, 0); // Only compare date, not time
+                                                    return date < today;
+                                                }}
+                                                initialFocus
+                                            />
+                                        </PopoverContent>
+                                    </Popover>
+                                    <FormMessage/>
+                                </FormItem>
+                            )}
+                        />
+                         <FormField
+                            control={form.control}
+                            name="timeslot"
+                            render={({ field }) => (
+                                <FormItem className="col-span-2 sm:col-span-1">
+                                    <FormControl>
+                                        <select {...field} className="w-full border p-2 bg-background text-muted-foreground">
+                                            <option value="">Select a timeslot</option>
+                                            {availableTimeSlots.map((slot) => (
+                                                <option key={slot} value={slot}>
+                                                    {slot}
+                                                </option>
+                                            ))}
+                                        </select>
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+
+                        <Button type="submit" className="col-span-2">
+                            Submit
+                        </Button>
                     </CardContent>
                 </form>
             </Form>
