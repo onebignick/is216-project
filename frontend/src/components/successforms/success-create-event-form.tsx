6f5a23c6b40eda6cb82db764db5db5bbeba2fe95
xinlin2 <xl.cheng.2023@scis.smu.edu.sg>
2024-11-05 10:40:12 +0800

    update success create event form

--- frontend/src/components/successforms/success-create-event-form.tsx
+++ frontend/src/components/successforms/success-create-event-form.tsx
@@ -18,26 +18,25 @@ export function SuccessCreateEventForm() {
     }, []);
     
     return (
-        <div className="flex justify-center items-center h-full vertical-align:middle">
-            <div className="flex flex-col items-center p-2">
-                <div className="flex flex-col items-center text-center text-3xl text-green-500 font-bold ">
+        <div className="flex items-center justify-center h-screen">
+            <div className="bg-white p-6 rounded text-center">
+                <div className="flex flex-col items-center text-3xl text-green-500 font-bold ">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-16 h-16">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                     </svg>
-                    <br />
-                    <h1>You have Successfully Created An Event.</h1>
-                    <br />
-                    <h1>Confirmation Code: {eventCode}</h1> {/* Display the event code */}
-                    <br />
-                    <h1>Your Zoom Link is: </h1>
                 </div>
-                <br/>
-                <div>
-                    <Button variant="outline" asChild>
-                        <Link href="/event">See More at Your Events</Link>
-                    </Button>
-                </div>
+                
+                <h1 className="text-3xl font-bold">You have successfully created an event!</h1>
+                <br></br>
+                <h1 className="text-2xl">Confirmation Code: {eventCode}</h1> {/* Display the event code */}
+                <br></br>
+                <h1 className="text-2xl">Your Zoom Link is: </h1>
+                <br></br>
+                <Button variant="outline" asChild>
+                    <Link href="/event">See More at Your Events</Link>
+                </Button>
             </div>
         </div>
+
     );
 }
