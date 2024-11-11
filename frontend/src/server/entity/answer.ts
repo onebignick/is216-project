05e35607d805bb30bf7339618923c615213cb6bb
onebignick <nicholas.zhengkai.ong@gmail.com>
2024-11-12 00:50:32 +0800

    remove unused files and clean dir

--- frontend/src/server/entity/answer.ts
+++
@@ -1,9 +0,0 @@
-export type MeetgridAnswer = {
-    id?: string;
-    noteId: string;
-    answer: string;    
-    createdAt?: Date;
-    updatedAt?: string | null;
-    participantId?: string | null;
-
-}
