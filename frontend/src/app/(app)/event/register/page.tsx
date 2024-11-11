af7f4f9f1fe79248cc37ec002a4c7f53f62d6a26
onebignick <nicholas.zhengkai.ong@gmail.com>
2024-11-11 09:38:04 +0800

    fix register page

---
+++ frontend/src/app/(app)/event/register/page.tsx
@@ -0,0 +1,9 @@
+import { RegisterEventForm } from "@/components/forms/register-event-form";
+
+export default function Event() {
+    return (
+        <div className="p-4">
+            <RegisterEventForm/>
+        </div>
+    )
+}
