e7fb2b86f49012d8e4996807e76bd2d959ea61da
xinlin2 <xl.cheng.2023@scis.smu.edu.sg>
2024-11-13 10:08:42 +0800

    update calendar and timeslot

--- frontend/src/components/forms/RegisterForTimeslotForm.tsx
+++ frontend/src/components/forms/RegisterForTimeslotForm.tsx
@@ -51,93 +51,107 @@ export function RegisterForTimeslotForm({ mergedAvailability, timeslotIdx, dayId
     async function onSubmit(values: z.infer<typeof formSchema>) {
         setIsLoading(true);
 
-        let eventParticipantEmailToFind = "";
-        for (const key in mergedAvailability[timeslotIdx][dayIdx]) {
-            if (mergedAvailability[timeslotIdx][dayIdx][key] != "") continue;
-            mergedAvailability[timeslotIdx][dayIdx][key] = values.email;
-            eventParticipantEmailToFind = key;
-            break;
-        }
+        try {
+                let eventParticipantEmailToFind = "";
+                for (const key in mergedAvailability[timeslotIdx][dayIdx]) {
+                    if (mergedAvailability[timeslotIdx][dayIdx][key] != "") continue;
+                    mergedAvailability[timeslotIdx][dayIdx][key] = values.email;
+                    eventParticipantEmailToFind = key;
+                    break;
+                }
 
-        console.log(eventParticipantEmailToFind)
+                console.log(eventParticipantEmailToFind)
 
-        //find the guy
-        const targetUserResponse = await fetch("/api/user?" + new URLSearchParams({
-            email: eventParticipantEmailToFind
-        }));
+                //find the guy
+                const targetUserResponse = await fetch("/api/user?" + new URLSearchParams({
+                    email: eventParticipantEmailToFind
+                }));
 
-        const { targetUser } = await targetUserResponse.json();
+                const { targetUser } = await targetUserResponse.json();
 
-        // find the guys availability;
-        const eventParticipantResponse = await fetch("/api/eventParticipant?" + new URLSearchParams({
-            eventId: event.id!,
-            userId: targetUser.clerkUserId
-        }))
-        const { eventParticipants } = await eventParticipantResponse.json()
+                // find the guys availability;
+                const eventParticipantResponse = await fetch("/api/eventParticipant?" + new URLSearchParams({
+                    eventId: event.id!,
+                    userId: targetUser.clerkUserId
+                }))
+                const { eventParticipants } = await eventParticipantResponse.json()
 
-        // update the guys availability
-        const eventParticipantToUpdate: MeetgridEventParticipant = eventParticipants[0];
-        const eventParticipantToUpdateAvailability = JSON.parse(eventParticipantToUpdate.availabilityString);
-        eventParticipantToUpdateAvailability[timeslotIdx][dayIdx][eventParticipantEmailToFind] = values.email;
-        eventParticipantToUpdate.availabilityString = JSON.stringify(eventParticipantToUpdateAvailability)
+                if (eventParticipantEmailToFind == values.email) {
+                    toast({
+                        title: "Unable to register for event",
+                        description: "Same user account register for the same event",
+                        className: "bg-red-500 text-black",
+                    })
+                    setIsLoading(false);
+                    router.push(pathname);
 
-        const eventParticipantToUpdateResponse = await fetch("/api/eventParticipant", {
-            method: "PUT",
-            body: JSON.stringify(eventParticipantToUpdate)
-        })
-        console.log(await eventParticipantToUpdateResponse.json());
-        
-        // todo generate zoom code and email
-        const eventStartDate = new Date(event.startDate);
-        eventStartDate.setDate(eventStartDate.getDate() + dayIdx);
-        eventStartDate.setMinutes(eventStartDate.getMinutes() + timeslotIdx*30)
-        const createZoomMeetingResponse = await fetch("/api/zoom", {
-            method: "POST",
-            body: JSON.stringify({
-                agenda: "Interview",
-                topic: event.name,
-                duration: 15,
-                startDateTime: formatDateToISOString(eventStartDate),
-            })
-        })
+                } else {        
+                    // update the guys availability
+                    const eventParticipantToUpdate: MeetgridEventParticipant = eventParticipants[0];
+                    const eventParticipantToUpdateAvailability = JSON.parse(eventParticipantToUpdate.availabilityString);
+                    eventParticipantToUpdateAvailability[timeslotIdx][dayIdx][eventParticipantEmailToFind] = values.email;
+                    eventParticipantToUpdate.availabilityString = JSON.stringify(eventParticipantToUpdateAvailability)
 
-        const { createdMeeting } = await createZoomMeetingResponse.json()
-        if (eventParticipantEmailToFind == values.email) {
+                    const eventParticipantToUpdateResponse = await fetch("/api/eventParticipant", {
+                        method: "PUT",
+                        body: JSON.stringify(eventParticipantToUpdate)
+                    })
+                    console.log(await eventParticipantToUpdateResponse.json());
+                    
+                    // todo generate zoom code and email
+                    const eventStartDate = new Date(event.startDate);
+                    eventStartDate.setDate(eventStartDate.getDate() + dayIdx);
+                    eventStartDate.setMinutes(eventStartDate.getMinutes() + timeslotIdx*30)
+                    const createZoomMeetingResponse = await fetch("/api/zoom", {
+                        method: "POST",
+                        body: JSON.stringify({
+                            agenda: "Interview",
+                            topic: event.name,
+                            duration: 30,
+                            startDateTime: formatDateToISOString(eventStartDate),
+                        })
+                    })
+                    const { createdMeeting } = await createZoomMeetingResponse.json()
+
+                    // create new event registrant at with that timeslot
+                    const eventRegistrantToCreate = {
+                        interviewerEmail: eventParticipantEmailToFind,
+                        participantName: values.participantName,
+                        participantEmail: values.email,
+                        eventId: event.id,
+                        timeslotIdx: timeslotIdx,
+                        dayIdx: dayIdx,
+                        zoomLink: createdMeeting.start_url
+                    } as MeetgridEventRegistrant
+
+                    // record the info in db
+                    const createdEventRegistrantResponse = await fetch("/api/eventRegistrant", {
+                        method: "POST",
+                        body: JSON.stringify(eventRegistrantToCreate),
+                    })
+                    console.log(await createdEventRegistrantResponse.json())
+
+                    toast({
+                        title: "Successfully Registered for event",
+                        description: "You will receive an email with the details of the meeting",
+                        className: "bg-green-500 text-black",
+                    })
+                }
+
+            } catch (error) {
+            console.error("Error during registration:", error);
             toast({
-                title: "Unable to register for event",
-                description: "Same user account register for the same event",
+                title: "Error",
+                description: "There was an issue with your registration. Please try again.",
                 className: "bg-red-500 text-black",
-            })
-        } else {
-            // create new event registrant at with that timeslot
-            const eventRegistrantToCreate = {
-                interviewerEmail: eventParticipantEmailToFind,
-                participantName: values.participantName,
-                participantEmail: values.email,
-                eventId: event.id,
-                timeslotIdx: timeslotIdx,
-                dayIdx: dayIdx,
-                zoomLink: createdMeeting.start_url
-            } as MeetgridEventRegistrant
-
-            // record the info in db
-            const createdEventRegistrantResponse = await fetch("/api/eventRegistrant", {
-                method: "POST",
-                body: JSON.stringify(eventRegistrantToCreate),
-            })
-            console.log(await createdEventRegistrantResponse.json())
-
-            toast({
-                title: "Successfully Registered for event",
-                description: "You will receive an email with the details of the meeting",
-                className: "bg-green-500 text-black",
-            })
-        }
+        });
+    } finally {
         setIsLoading(false);
         router.push(pathname);
-        
+        router.refresh();
     }
 
+    } 
     return(
             <Form {...form}>
                 <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
